910111fed951de0f988b85a0a29ee70cf479e4d7
Milka44 <po4tafor4@gmail.com>
2024-05-19 20:49:20 +0300

    HW 5 is done

---
+++ jsConditions/task3.js
@@ -0,0 +1,15 @@
+
+//Завдання 3: Генерація таблиці множення
+
+const a = 2;
+for (let b = 1; b <= 10; b++) {
+    console.log(`2 * ${b} = ${a * b}`);
+    //console.log(a, "*" ,b,"=",a * b);
+}
+
+const c = 2;
+let d = 1;
+while (d <= 10) {
+    console.log(`${c} * ${d} = ${c*d}`);
+    d++;
+}
