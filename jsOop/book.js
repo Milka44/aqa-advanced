a5005ef5839e0ae1cbf8d92da13d3fdb33fae7da
Milka44 <po4tafor4@gmail.com>
2024-06-06 12:47:54 +0300

    HW 10 is done

---
+++ jsOop/book.js
@@ -0,0 +1,54 @@
+class Book {
+  static 
+      arrayOfBooks(...books){
+      return books;
+      }
+  
+  constructor(bookTitle, bookAuthor, issuedDate) {
+    this.bookTitle = bookTitle;
+    this.bookAuthor = bookAuthor;
+    this.issuedDate = issuedDate;
+  }
+
+  get bookTitle() {
+      return this._bookTitle;
+  }
+
+  set bookTitle(value) {
+      if (typeof value !== 'string' || !value) {
+          console.error("Error: Enter correct book name.");
+        return;
+      }
+      this._bookTitle = value;
+    };
+
+    get bookAuthor() {
+      return this._bookAuthor;
+  }
+
+  set bookAuthor(value) {
+      if (typeof value !== 'string' || !value) {
+          console.error("Error: Enter correct author name.");
+        return;
+      }
+      this._bookAuthor = value;
+    };
+  get issuedDate() {
+      return this._issuedDate;
+  }
+
+  set issuedDate(value) {
+      if (typeof value !== 'number' || value > 2024) {
+        console.error("Error: Date of issue can not be more than 2024.");
+        return;
+      }
+      this._issuedDate = value;
+    }
+
+  printInfo() {
+    console.log(`This book is ${this.bookTitle}, written by ${this.bookAuthor}, was published in ${this.issuedDate}`);
+  }
+}
+
+
+module.exports = Book;
---
+++ jsOop/task1.js
@@ -0,0 +1,49 @@
+const Book = require('./book');
+const Ebook = require('./EBook');
+
+
+//Book
+const book1 = new Book('1984', 'George Orwell', 1949);
+book1.printInfo();
+  
+const book2 = new Book('Great expectations', 'Charles Dickens', 1861);
+book2.printInfo();
+  
+const book3 = new Book('Fahrenheit 451', 'Ray Bradbury', 1953);
+book3.printInfo()
+
+const book4 = new Book([],0, 2020);// should throw Error
+book4.printInfo()
+
+const allBooksArray = Book.arrayOfBooks(book1, book2, book3, book4);
+
+//Ebook
+
+const ebook1 = new Ebook('1985', 'George Orwell', 1950, '.pdf');
+ebook1.printInfo();
+const ebook2 = new Ebook('Fahrenheit 451', 'Ray Bradbury', 1953,'.txt' );
+ebook2.printInfo();
+
+const allEbooksArray = Ebook.arrayOfEbooks(ebook1, ebook2);
+
+//4.Створіть статичний метод в класі Book, який буде приймати масив об'єктів(екземрлярів) книг та повертати найдавнішу книгу за роком видання. 
+//Викличте його в коді передавши масив книг (серед них мають бути екземляри обох класів Book та EBook)
+
+const copiedArray = [...allBooksArray, ...allEbooksArray];
+
+// searching book with the earliest date//accumulator=earliest
+function getEarliestBook(array, field) {
+    return array.reduce((earliest, current) => {
+        return (earliest[field] < current[field]) ? earliest : current;
+    });
+}
+
+const earliestBook = getEarliestBook(copiedArray, '_issuedDate');
+console.log(earliestBook);
+
+//5.Створіть статичний метод для EBook який буде приймати як аргументи екземпляр 
+//класу Book і формат файлу як рядок ****та повертати екземпляр класу EBook
+
+const ebookFromBook = Ebook.arrayOfBooks(book1, 'csv');
+ebookFromBook.printInfo();
+
